a1265e3820172ddf8253b70db79c91cdef31f892
Sungho Park <siminsungho@gmail.com>
2020-10-30 00:37:56 +0900

    refactoring

--- source/ubinos/bsp/arch/arm/cortexm/nrf52dk/dtty_nrf_libuarte.c
+++ source/ubinos/bsp/arch/arm/cortexm/nrf52dk/dtty_nrf_libuarte.c
@@ -24,7 +24,6 @@
 #include <assert.h>
 
 #include <boards.h>
-#include <app_error.h>
 
 #include "nrf_libuarte_async.h"
 
@@ -34,22 +33,15 @@ extern int _g_bsp_dtty_init;
 extern int _g_bsp_dtty_echo;
 extern int _g_bsp_dtty_autocr;
 
-NRF_LIBUARTE_ASYNC_DEFINE(_g_dtty_async_libuarte, 0, 0, 0, NRF_LIBUARTE_PERIPHERAL_NOT_USED, 255, 3);
+#define DTTY_NRF_LIBUARTE_READ_BUFFER_SIZE 256
 
-#define DTTY_ASYNC_READ_BUFFER_SIZE 256
+NRF_LIBUARTE_ASYNC_DEFINE(_g_dtty_nrf_libuarte, 0, 0, 0, NRF_LIBUARTE_PERIPHERAL_NOT_USED, 255, 3);
 
-typedef struct _dtty_async_buffer_t
-{
-    uint8_t data[DTTY_ASYNC_READ_BUFFER_SIZE];
-    uint16_t head;
-    uint16_t tail;
-} dtty_async_buffer_t;
+cbuf_def_init(_g_dtty_nrf_libuarte_rbuf, DTTY_NRF_LIBUARTE_READ_BUFFER_SIZE);
 
-dtty_async_buffer_t _g_dtty_async_read_buffer;
+sem_pt _g_dtty_nrf_libuarte_read_sem = NULL;
 
-sem_pt _g_dtty_async_read_sem = NULL;
-
-static void dtty_async_event_handler(void *context, nrf_libuarte_async_evt_t *p_evt)
+static void dtty_nrf_libuarte_event_handler(void *context, nrf_libuarte_async_evt_t *p_evt)
 {
     int need_signal = 0;
 
@@ -59,20 +51,16 @@ static void dtty_async_event_handler(void *context, nrf_libuarte_async_evt_t *p_
         break;
 
     case NRF_LIBUARTE_ASYNC_EVT_RX_DATA:
-        if (_g_dtty_async_read_buffer.tail == _g_dtty_async_read_buffer.head)
+        if (cbuf_get_len(_g_dtty_nrf_libuarte_rbuf) == 0)
         {
             need_signal = 1;
         }
 
-        for (size_t i = 0; i < p_evt->data.rxtx.length; i++)
-        {
-            _g_dtty_async_read_buffer.data[_g_dtty_async_read_buffer.tail] = p_evt->data.rxtx.p_data[i];
-            _g_dtty_async_read_buffer.tail = (_g_dtty_async_read_buffer.tail + 1) % DTTY_ASYNC_READ_BUFFER_SIZE;
-        }
+        cbuf_write(_g_dtty_nrf_libuarte_rbuf, p_evt->data.rxtx.p_data, p_evt->data.rxtx.length, NULL);
 
         if (need_signal && _bsp_kernel_active)
         {
-            sem_give(_g_dtty_async_read_sem);
+            sem_give(_g_dtty_nrf_libuarte_read_sem);
         }
         break;
 
@@ -91,10 +79,7 @@ int dtty_init(void)
 
     if (!_g_bsp_dtty_init && !bsp_isintr() && _bsp_kernel_active)
     {
-        _g_dtty_async_read_buffer.head = 0;
-        _g_dtty_async_read_buffer.tail = 0;
-
-        r = semb_create(&_g_dtty_async_read_sem);
+        r = semb_create(&_g_dtty_nrf_libuarte_read_sem);
         assert(r == 0);
 
         nrf_libuarte_async_config_t nrf_libuarte_async_config =
@@ -107,11 +92,10 @@ int dtty_init(void)
                 .timeout_us = 100,
                 .int_prio = APP_IRQ_PRIORITY_LOW };
 
-        err_code = nrf_libuarte_async_init(&_g_dtty_async_libuarte, &nrf_libuarte_async_config, dtty_async_event_handler, (void*) &_g_dtty_async_libuarte);
-
+        err_code = nrf_libuarte_async_init(&_g_dtty_nrf_libuarte, &nrf_libuarte_async_config, dtty_nrf_libuarte_event_handler, (void*) &_g_dtty_nrf_libuarte);
         APP_ERROR_CHECK(err_code);
 
-        nrf_libuarte_async_enable(&_g_dtty_async_libuarte);
+        nrf_libuarte_async_enable(&_g_dtty_nrf_libuarte);
 
         _g_bsp_dtty_echo = 1;
         _g_bsp_dtty_autocr = 1;
@@ -139,6 +123,8 @@ int dtty_geterror(void)
 
 int dtty_getc(char *ch_p)
 {
+    ubi_err_t ubi_err;
+
     if (!_g_bsp_dtty_init)
     {
         dtty_init();
@@ -146,22 +132,23 @@ int dtty_getc(char *ch_p)
 
     if (_g_bsp_dtty_init && !bsp_isintr())
     {
-        do
+        while (1)
         {
-            if (_g_dtty_async_read_buffer.head != _g_dtty_async_read_buffer.tail)
+            if (cbuf_get_len(_g_dtty_nrf_libuarte_rbuf) == 0)
             {
-                *ch_p = _g_dtty_async_read_buffer.data[_g_dtty_async_read_buffer.head];
-                _g_dtty_async_read_buffer.head = (_g_dtty_async_read_buffer.head + 1) % DTTY_ASYNC_READ_BUFFER_SIZE;
+                sem_take(_g_dtty_nrf_libuarte_read_sem);
+            }
+
+            ubi_err = cbuf_read(_g_dtty_nrf_libuarte_rbuf, (uint8_t*) ch_p, 1, NULL);
+            if (ubi_err == UBI_ERR_OK)
+            {
                 break;
             }
-            else
-            {
-                sem_take(_g_dtty_async_read_sem);
-            }
-        } while (1);
+        }
     }
 
-    if (0 != _g_bsp_dtty_echo) {
+    if (0 != _g_bsp_dtty_echo)
+    {
         dtty_putc(*ch_p);
     }
 
@@ -195,12 +182,11 @@ int dtty_putc(int ch)
 
         do
         {
-            ret = nrf_libuarte_async_tx(&_g_dtty_async_libuarte, buf, len);
+            ret = nrf_libuarte_async_tx(&_g_dtty_nrf_libuarte, buf, len);
             if (ret == NRF_ERROR_BUSY)
             {
                 continue;
             }
-
             APP_ERROR_CHECK(ret);
 
             break;
@@ -250,7 +236,7 @@ int dtty_kbhit(void)
 
     if (_g_bsp_dtty_init && !bsp_isintr())
     {
-        if (_g_dtty_async_read_buffer.head != _g_dtty_async_read_buffer.tail)
+        if (cbuf_get_len(_g_dtty_nrf_libuarte_rbuf) != 0)
         {
             r = 1;
         }
